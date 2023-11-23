4000b9b5121e3cca2d6c0e28824908ca6e16ce59
foxcreator <foxcreatorg@gmail.com>
2023-11-23 12:51:58 +0200

    fix sidebar

--- resources/views/front/layouts/aside-block.blade.php
+++ resources/views/front/layouts/aside-block.blade.php
@@ -6,23 +6,23 @@ $categories = \App\Models\Category::all();
 <div class="col-md-3">
     <!-- ======= Sidebar ======= -->
     <div class="aside-block">
-
         <ul class="nav nav-pills custom-tab-nav mb-4" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="pills-popular-tab" data-bs-toggle="pill" data-bs-target="#pills-popular" type="button" role="tab" aria-controls="pills-popular" aria-selected="true">Популярні</button>
+                <button class="nav-link active" id="pills-popular-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-popular" type="button" role="tab" aria-controls="pills-popular"
+                        aria-selected="true">Популярні
+                </button>
             </li>
-{{--            <li class="nav-item" role="presentation">--}}
-{{--                <button class="nav-link" id="pills-trending-tab" data-bs-toggle="pill" data-bs-target="#pills-trending" type="button" role="tab" aria-controls="pills-trending" aria-selected="false">Trending</button>--}}
-{{--            </li>--}}
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-latest-tab" data-bs-toggle="pill" data-bs-target="#pills-latest" type="button" role="tab" aria-controls="pills-latest" aria-selected="false">Останні</button>
+                <button class="nav-link" id="pills-latest-tab" data-bs-toggle="pill" data-bs-target="#pills-latest"
+                        type="button" role="tab" aria-controls="pills-latest" aria-selected="false">Останні
+                </button>
             </li>
         </ul>
 
         <div class="tab-content" id="pills-tabContent">
-
-            <!-- Popular -->
-            <div class="tab-pane fade show active" id="pills-popular" role="tabpanel" aria-labelledby="pills-popular-tab">
+            <div class="tab-pane fade show active" id="pills-popular" role="tabpanel"
+                 aria-labelledby="pills-popular-tab">
                 @foreach($popular as $article)
                     <div class="post-entry-1 border-bottom">
                         <div class="post-meta">
@@ -35,14 +35,13 @@ $categories = \App\Models\Category::all();
                             <span>{{ \Carbon\Carbon::parse($article->created_at)->translatedFormat('d F Y') }}</span>
                         </div>
 
-                        <h2 class="mb-2"><a href="{{ route('show.article', $article->id) }}">{{ $article->title }}</a></h2>
+                        <h2 class="mb-2"><a href="{{ route('show.article', $article->id) }}">{{ $article->title }}</a>
+                        </h2>
                         <span class="author mb-3 d-block">{{ $article->user?->name }}</span>
                     </div>
                 @endforeach
+            </div>
 
-            </div> <!-- End Popular -->
-
-            <!-- Latest -->
             <div class="tab-pane fade" id="pills-latest" role="tabpanel" aria-labelledby="pills-latest-tab">
                 @foreach($latest as $article)
                     <div class="post-entry-1 border-bottom">
@@ -50,37 +49,44 @@ $categories = \App\Models\Category::all();
                             @foreach($article->categories as $category)
                                 <span class="date">{{ $category->name }}</span>
                             @endforeach
-                            <span class="mx-1">&bullet;</span> <span>{{ \Carbon\Carbon::parse($article->created_at)->diffForHumans() }}</span></div>
-                        <h2 class="mb-2"><a href="{{ route('show.article', $article->id) }}">{{ $article->title }}</a></h2>
+                            <span class="mx-1">&bullet;</span>
+                            <span>{{ \Carbon\Carbon::parse($article->created_at)->diffForHumans() }}</span></div>
+                        <h2 class="mb-2"><a href="{{ route('show.article', $article->id) }}">{{ $article->title }}</a>
+                        </h2>
                         <span class="author mb-3 d-block">{{ $article->user?->name }}</span>
                     </div>
                 @endforeach
-
             </div>
-
         </div>
     </div>
+
     @if(request()->is('front/article/*'))
         @php
             $articleId = request()->segment(3);
             $currentArticle = \App\Models\Article::find($articleId);
         @endphp
-    <div class="aside-block">
-        <h3 class="aside-title">Відео</h3>
-        <div class="video-post">
-            <a href="https://www.youtube.com/watch?v=AiFfDjmd0jU" class="glightbox link-video">
-                <span class="bi-play-fill"></span>
-                <img src="{{ asset('assets/img/post-landscape-5.jpg') }}" alt="" class="img-fluid">
-            </a>
-        </div>
-    </div>
+        @if($currentArticle->video_url)
+            <div class="aside-block">
+                <h3 class="aside-title">Відео</h3>
+                <div class="video-post">
+                    <a href="{{ $currentArticle->video_url }}" class="glightbox link-video">
+                        <span class="bi-play-fill"></span>
+                        <img src="{{ asset($currentArticle->video_preview) }}" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+        @endif
     @endif
 
     <div class="aside-block">
         <h3 class="aside-title">Категорії</h3>
         <ul class="aside-links list-unstyled">
             @foreach($categories as $category)
-                <li><a href="{{ route('category.post', $category->id) }}"><i class="bi bi-chevron-right"></i> {{ $category->name }}</a></li>
+                <li>
+                    <a href="{{ route('category.post', $category->id) }}">
+                        <i class="bi bi-chevron-right"></i> {{ $category->name }}
+                    </a>
+                </li>
             @endforeach
         </ul>
     </div>
